0a5d1bc96e1ef19f3dc24c5a552c703d21afd1f3
ChristopheHunter <18363853560@163.com>
2017-06-23 23:44:48 +0800

    javascript的学习（1）

---
+++ web/day04/test.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zy_git on 2017/6/23.
+ */
+function myFun() {
+//            document.getElementById("demo").innerHTML = "我的第一个js函数";
+    alert("hahaha")
+}
+
+function myFun2() {
+    document.getElementById("demo").innerHTML = "我的第二个js函数"
+}
+
+function myFun3() {
+    var a = 10 + parseInt('a');
+    document.writeln(a)
+}
+function myFun4() {
+    var s = "66";
+    var i =  66;
+    alert(s === i);
+}
